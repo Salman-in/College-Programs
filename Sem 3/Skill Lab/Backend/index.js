63c1f8e5e03a2370a10ef25c2a0d429125d1ec1b
Salman <salmannajah123@gmail.com>
2025-01-15 10:15:48 +0530

    Changes made to files

--- Sem 3/Skill Lab/Backend/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-let abc = http.createServer((req, res) => {
-    res.write("Hey!")
-    res.end()
-    console.log("Server is running on port 3000");
-
-    abc.listen(3000);
-});
--- Sem 3/Skill Lab/skill-lab-react/src/Feedback.jsx
+++
@@ -1,6 +0,0 @@
-export default Feedback() {
-
-    return <div>
-    Feedback
-    </div>
-}
